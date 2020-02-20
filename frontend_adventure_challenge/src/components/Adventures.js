5a9ff9ffcb0c6902d0e624731d053c290701540a
github username <github email address>
2020-02-20 09:48:03 -0800

    creating Adventures component

---
+++ frontend_adventure_challenge/src/components/Adventures.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Adventure from './Adventure'
+
+const Adventures = props => {
+
+    const adventures = props.adventures.map(adventure => <Adventure key={adventure.id} adventure={adventure} />)
+
+    return (
+        <div>
+            {adventures}
+        </div>
+    )
+}
+
+export default Adventures;
