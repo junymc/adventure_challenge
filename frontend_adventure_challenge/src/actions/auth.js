3635b07a7ee30e42a99c1dea44c26c696c73dbf8
github username <github email address>
2020-03-01 20:02:25 -0800

    setting fetch for user token

---
+++ frontend_adventure_challenge/src/actions/auth.js
@@ -0,0 +1,22 @@
+export const getToken = () => {
+    return async function (dispatch) {
+        try{
+            const res = await fetch("http://localhost:3001/api/vi/auth", {
+                credentials: 'include'
+            })
+            if(!res.ok){
+                throw res
+            }
+            const tkJson = await res.json()
+            const csrf_token = tkJson.csrf_auth_token
+            console.log(tkJson)
+            console.log(csrf_token)
+            dispatch({
+                type: 'GET_CSRF_TOKEN',
+                payload: csrf_token
+            })
+        }catch(error){
+            console.log(error)
+        }
+    }
+}
