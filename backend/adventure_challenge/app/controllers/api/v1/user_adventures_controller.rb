020103899b0d9fc8c4875c59209d6b0960cd662d
github username <github email address>
2020-03-05 10:58:15 -0800

    adding UserAdventuresController

--- backend/adventure_challenge/app/controllers/api/v1/user_adventures_controller.rb
+++ backend/adventure_challenge/app/controllers/api/v1/user_adventures_controller.rb
@@ -1,7 +1,18 @@
 class UserAdventuresController < ApplicationController
 
     def create
-
+        @user_adventure = UserAdventure.new(user_adventure_params)   
+        if @user_adventure.save
+            render json: @user_adventure, status: 200
+        else
+            render json: {error: "Something went wrong, please try again."}, status: 400
+        end
     end
-    
+
+    private
+
+    def user_adventure_params
+        params.require(:user_adventure).permit(:user_id, :adventure_id)
+    end
+   
 end
