a62b43d000238dfc6af3a8c53d2e484abe267f96
github username <github email address>
2020-05-21 14:19:57 -0700

    adding current_user back to users controller and route

--- backend/adventure_challenge/app/controllers/api/v1/users_controller.rb
+++ backend/adventure_challenge/app/controllers/api/v1/users_controller.rb
@@ -10,12 +10,12 @@ class Api::V1::UsersController < ApplicationController
         end
     end
 
-    # def current_user
-    #     @user = User.find_by(id: session[:user_id])
-    #     if @user
-    #         render json: @user, except: [:password_digest, :created_at, :updated_at], status: 200
-    #     end
-    # end
+    def current_user
+        @user = User.find_by(id: session[:user_id])
+        if @user
+            render json: @user, except: [:password_digest, :created_at, :updated_at], status: 200
+        end
+    end
 
 
     private
